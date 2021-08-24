2143192e2f2f108b97e93b906b44c7bb965c2ab8
Derryn <derryn.harvie@gmail.com>
2021-08-24 18:14:52 +1000

    Added a complied DFU file for test only.

--- MIDI_Commander_Custom/DFU Release/Core/Startup/subdir.mk
+++ MIDI_Commander_Custom/DFU Release/Core/Startup/subdir.mk
@@ -15,6 +15,6 @@ S_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Startup/startup_stm32f103retx.o: ../Core/Startup/startup_stm32f103retx.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -I"D:/midi-commander-custom/MIDI_Commander_Custom/Middlewares/stm32-ssd1306-master/ssd1306" -I"D:/midi-commander-custom/MIDI_Commander_Custom/Middlewares/ST/STM32_USB_Device_Library/Class/MIDI/Inc" -x assembler-with-cpp -MMD -MP -MF"Core/Startup/startup_stm32f103retx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -I"D:/midi-commander-custom/MIDI_Commander_Custom/Middlewares/stm32-ssd1306-master/ssd1306" -I"D:/midi-commander-custom/MIDI_Commander_Custom/Middlewares/ST/STM32_USB_Device_Library/Class/MIDI/Inc" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 
